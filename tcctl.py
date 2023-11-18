a99accd08d1e952dc40e022de3e8496bc2dc07d7
3DRX <kjy20030228@icloud.com>
2023-11-18 16:41:43 +0800

    Add tcctl.py

---
+++ tcctl.py
@@ -0,0 +1,66 @@
+import os
+import signal
+import sys
+from typing import List
+
+
+NIC = "br-lan"
+
+
+def on_exit(signum, frame):
+    """
+    unset bandwidth limit when exiting
+    """
+    print()
+    print("exiting, unset bandwidth limit")
+    os.system(f"tc qdisc del dev {NIC} root")
+    sys.exit(0)
+    pass
+
+
+def parse(path: str) -> List[float]:
+    trace: List[float] = []
+    with open(path, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if len(line) > 0:
+                delay, loss, bandwidth = list(map(float, line.split(" ")))
+                trace.append(bandwidth)
+                pass
+            pass
+        pass
+    return trace
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("usage: sudo python3 tcscript.py <trace file path>")
+        sys.exit(0)
+        pass
+    if not os.path.exists(sys.argv[1]):
+        print("error: trace file not found")
+        sys.exit(0)
+        pass
+    if os.geteuid() != 0:
+        print("error: permission denied (this program need to be run as root)")
+        sys.exit(0)
+        pass
+    signal.signal(signal.SIGINT, on_exit)
+    signal.signal(signal.SIGTERM, on_exit)
+    trace: List[float] = parse(sys.argv[1])
+    first: bool = True
+    length: int = len(trace)
+    os.system(f"sudo tc qdisc del dev {NIC} root")
+    for i, t in enumerate(trace):
+        t = max(2, t)
+        print(f"[{i+1}/{length}] limiting bandwidth to {t}Mbps")
+        if first:
+            os.system(f"sudo tc qdisc add dev {NIC} root netem rate {t}mbit")
+            first = False
+        else:
+            os.system(
+                f"sudo tc qdisc change dev {NIC} root netem rate {t}mbit")
+            pass
+        pass
+    os.system(f"tc qdisc del dev {NIC} root")
+    pass
