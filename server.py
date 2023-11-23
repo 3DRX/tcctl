bf6c4c0d0ee216b8850238d2acef0644cebcde7b
3DRX <kjy20030228@icloud.com>
2023-11-23 22:08:00 +0800

    refactor backend
    1. add proper logging
    2. optimize logic in server.py

--- server.py
+++ server.py
@@ -2,9 +2,35 @@ from flask import Flask, request, send_from_directory
 from tcctl import NetemController
 import psutil
 import os
+from logging.config import dictConfig
+from typing import Dict
 
-app = Flask(__name__, static_folder="./webui/dist/")
-controllers = []
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            },
+            "custom_handler": {
+                "class": "logging.FileHandler",
+                "formatter": "default",
+                "filename": "tcctl.log",
+            },
+        },
+        "root": {"level": "INFO", "handlers": ["wsgi", "custom_handler"]},
+    }
+)
+
+app: Flask = Flask(__name__, static_folder="./dist/")
+controllers: Dict[str, NetemController] = {}
 
 
 @app.route("/api/v1/interfaces", methods=["POST"])
@@ -17,27 +43,25 @@ def put_netem():
     body = request.get_json()
     # sanity check
     if "NIC" not in body:
+        app.logger.warning(f"missing NIC in {body}")
         return "missing NIC", 400
     if "delay" not in body:
+        app.logger.warning(f"missing delay in {body}")
         return "missing delay", 400
     if "loss" not in body:
+        app.logger.warning(f"missing loss in {body}")
         return "missing loss", 400
     if "rate" not in body:
+        app.logger.warning(f"missing rate in {body}")
         return "missing rate", 400
     if body["NIC"] not in psutil.net_io_counters(pernic=True):
+        app.logger.warning(f"invalid NIC {body['NIC']}")
         return "invalid NIC", 400
     # set netem
-    for ctl in controllers:
-        if ctl.NIC == body["NIC"]:
-            ctl.set(
-                float(body["delay"]),
-                float(body["loss"]),
-                float(body["rate"]),
-            )
-            return ""
+    if body["NIC"] not in controllers:
+        controllers[body["NIC"]] = NetemController(body["NIC"], app)
         pass
-    controllers.append(NetemController(body["NIC"]))
-    controllers[-1].set(
+    controllers[body["NIC"]].set(
         float(body["delay"]),
         float(body["loss"]),
         float(body["rate"]),
@@ -55,5 +79,5 @@ def serve(path):
 
 
 if __name__ == "__main__":
-    app.run(debug=False, port=8080)
+    app.run(debug=True, port=8080)
     pass
--- tcctl.py
+++ tcctl.py
@@ -1,44 +1,22 @@
 import os
-import signal
-import time
-import sys
+from flask import Flask
 from typing import List
 
 
-NIC = "br-lan"
-
-
-def on_exit(signum, frame):
-    """
-    unset bandwidth limit when exiting
-    """
-    print()
-    print("exiting, unset bandwidth limit")
-    os.system(f"tc qdisc del dev {NIC} root")
-    sys.exit(0)
-    pass
-
-
-def parse_trace(path: str) -> List[float]:
-    trace: List[float] = []
-    with open(path, "r") as f:
-        for line in f.readlines():
-            line = line.strip()
-            if len(line) > 0:
-                delay, loss, bandwidth = list(map(float, line.split(" ")))
-                trace.append(bandwidth)
-                pass
-            pass
-        pass
-    return trace
-
-
 class NetemController(object):
-    def __init__(self, NIC: str):
+    def __init__(self, NIC: str, app: Flask):
         self.NIC = NIC
         self.__first = True
-        os.system(f"tc qdisc del dev {NIC} root")
+        self.__app = app
+        app.logger.info(f"create NetemController for {NIC}")
         pass
+
+    def __success(self, result: str) -> bool:
+        # TODO
+        parsed: List[str] = result.split("\n").pop()
+        if len(parsed) == 0:
+            return False
+        return True
 
     def set(self, delay: float, loss: float, bandwidth: float):
         # if any of the 3 values is below 0, unset netem
@@ -47,57 +25,34 @@ class NetemController(object):
             return
         operation = "add" if self.__first else "change"
         self.__first = False
-        os.system(
+        result = os.popen(
             f"tc qdisc {operation} dev {self.NIC} root netem delay {delay}ms loss {loss}% rate {bandwidth}mbit"
-        )
-        print(f"set netem delay {delay}ms loss {loss}% rate {bandwidth}mbit")
+        ).read()
+        if self.__success(result):
+            self.__app.logger.info(
+                f"set delay {delay}ms loss {loss}% rate {bandwidth}mbit"
+            )
+        else:
+            self.__app.logger.error(
+                f"failed to set delay {delay}ms loss {loss}% rate {bandwidth}mbit"
+            )
+            pass
         pass
 
     def unset(self):
-        os.system(f"tc qdisc del dev {self.NIC} root")
+        result = os.popen(f"tc qdisc del dev {self.NIC} root").read()
+        if self.__success(result):
+            self.__app.logger.info(f"unset netem limit of {self.NIC}")
+        else:
+            self.__app.logger.error(
+                f"failed to unset netem limit of {self.NIC}")
+            pass
         self.__first = True
-        print(f"unset netem limit of {self.NIC}")
         pass
 
     def __del__(self):
-        os.system(f"tc qdisc del dev {self.NIC} root")
-        print(f"unset netem limit of {self.NIC}")
+        self.unset()
+        self.__app.logger.info(f"delete NetemController for {self.NIC}")
         pass
 
-    pass
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("usage: sudo python3 tcscript.py <trace file path>")
-        sys.exit(0)
-        pass
-    if not os.path.exists(sys.argv[1]):
-        print("error: trace file not found")
-        sys.exit(0)
-        pass
-    if os.geteuid() != 0:
-        print("error: permission denied (this program need to be run as root)")
-        sys.exit(0)
-        pass
-    signal.signal(signal.SIGINT, on_exit)
-    signal.signal(signal.SIGTERM, on_exit)
-    trace: List[float] = parse_trace(sys.argv[1])
-    first: bool = True
-    length: int = len(trace)
-    ctl = NetemController(NIC)
-    last_bandwidth: float = 0.0
-    for i, t in enumerate(trace):
-        t = max(2, t)
-        if t == last_bandwidth:
-            time.sleep(1)
-            continue
-        else:
-            last_bandwidth = t
-            pass
-        print(f"[{i+1}/{length}] limiting bandwidth to {t}Mbps")
-        ctl.set(0, 0, t)
-        time.sleep(1)
-        pass
-    os.system(f"tc qdisc del dev {NIC} root")
     pass
