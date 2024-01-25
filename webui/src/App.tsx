064b4eb717e85012023957a07fb66fbad09d66a3
3DRX <kjy20030228@icloud.com>
2024-01-25 22:23:41 +0800

    Add prettier as dev dep, add dark mode switch

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import History from "./components/History";
+import { ConfigProvider, FloatButton, theme } from "antd";
 
 function App() {
+  const [dark, setdark] = useState(localStorage.getItem("dark") === "true");
+
+  useEffect(() => {
+    localStorage.setItem("dark", JSON.stringify(dark));
+    document.documentElement.style.setProperty(
+      "--color-background",
+      dark ? "black" : "white",
+    );
+  }, [dark]);
+
   return (
-    <>
-      <History />
-    </>
+    <div
+      style={{
+        backgroundColor: dark ? "black" : "white",
+      }}
+    >
+      <ConfigProvider
+        theme={{
+          algorithm: dark ? theme.darkAlgorithm : theme.defaultAlgorithm,
+        }}
+      >
+        <History />
+        <FloatButton
+          onClick={(_) => {
+            setdark(!dark);
+          }}
+          icon={dark ? "☀️" : "🌙"}
+        />
+      </ConfigProvider>
+    </div>
   );
 }
 
